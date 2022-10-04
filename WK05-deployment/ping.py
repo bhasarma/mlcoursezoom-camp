7e1aca1b89949de4ecd70749c9a221a409f70900
Bhaskar Sarma <sarma.bhaskarjyoti@gmail.com>
2022-10-04 15:32:34 +0200

    update

---
+++ WK05-deployment/ping.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask('ping')
+
+@app.route('/ping', methods = ['GET']) #this is a function decorator. 
+# google 'get post web service'. We want to access this function usung the get method
+def ping():
+	return "PONG"
+
+if __name__=="__main__":
+	app.run(debug = True, host='0.0.0.0', port = 9696)
